417c04de804987fa7a23a571f4f1fc5b0a766d62
24engiball <engiball@gmail.com>
2024-07-17 17:15:38 +0700

    first commit
    first commit

---
+++ basicC2/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1721211053,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/playbutton-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/player-sheet0.png",
+		"images/fruit-sheet0.png",
+		"images/menu-sheet0.png",
+		"images/replay-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
